7dc475c26e11ae6fef41ff8af9abdb5c6d400a93
kyhfan <kyhfan@gmail.com>
2018-08-21 14:20:30 +0900

    1111

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
+<?
+    include_once "./include/autoload.php";
+
+    $mnv_f 			= new mnv_function();
+    $my_db         = $mnv_f->Connect_MySQL();
+    $mobileYN      = $mnv_f->MobileCheck();
+    // $obYN          = $mnv_f->BrowserCheck();
+    $IEYN          = $mnv_f->IECheck();
+    $SafariYN          = $mnv_f->SafariCheck();
+    // print_r($_SERVER["HTTP_USER_AGENT"]);
+    if ($mobileYN == "MOBILE")
+    {
+        // echo "<script>location.href='m/index.php?media=".$_REQUEST["media"]."&r=".$_REQUEST["r"]."&ref=".$_REQUEST["ref"]."&w=".$_REQUEST["w"]."';</script>";
+    }else{
+        $saveMedia     = $mnv_f->SaveMedia();
+        $rs_tracking   = $mnv_f->InsertTrackingInfo($mobileYN);
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
