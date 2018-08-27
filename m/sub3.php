a13d90533fb439a5ef018d50e09f39b07ca2abd1
kyhfan <kyhfan@gmail.com>
2018-08-27 18:15:40 +0900

    4444

--- m/sub3.php
+++ m/sub3.php
@@ -142,15 +142,15 @@
 
 				localStorage.setItem("size",that.val());
 				if (localStorage.type == 1)
-					change_rs_img = "./files/" + localStorage.serial + "/maumbot_takecare" + that.val() + ".jpg";
+					change_rs_img = "../files/" + localStorage.serial + "/maumbot_takecare" + that.val() + ".jpg";
 				else if (localStorage.type == 2)
-					change_rs_img = "./files/" + localStorage.serial + "/maumbot_loveyou" + that.val() + ".jpg";
+					change_rs_img = "../files/" + localStorage.serial + "/maumbot_loveyou" + that.val() + ".jpg";
 				else if (localStorage.type == 3)
-					change_rs_img = "./files/" + localStorage.serial + "/maumbot_thanks" + that.val() + ".jpg";
+					change_rs_img = "../files/" + localStorage.serial + "/maumbot_thanks" + that.val() + ".jpg";
 				else if (localStorage.type == 4)
-					change_rs_img = "./files/" + localStorage.serial + "/maumbot_cheerup" + that.val() + ".jpg";
+					change_rs_img = "../files/" + localStorage.serial + "/maumbot_cheerup" + that.val() + ".jpg";
 				else if (localStorage.type == 5)
-					change_rs_img = "./files/" + localStorage.serial + "/maumbot_dontworry" + that.val() + ".jpg";
+					change_rs_img = "../files/" + localStorage.serial + "/maumbot_dontworry" + that.val() + ".jpg";
 
 				if ($("#"+that.attr('id')).is(":checked") === true)
 				{
