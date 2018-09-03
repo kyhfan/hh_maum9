0bdf45b7a628c871bb0d19b94c83a36f87826ffb
kyhfan <kyhfan@gmail.com>
2018-09-03 11:48:26 +0900

    22222

--- include/mnv_function.php
+++ include/mnv_function.php
@@ -14,7 +14,7 @@ class mnv_function extends mnv_dbi
 	public function InsertTrackingInfo($gubun)
 	{
 		global $my_db;
-		$log_query	= "INSERT INTO tracking_info(tracking_media, tracking_refferer, tracking_ipaddr, tracking_date, tracking_gubun) values('".$_SESSION['ss_media']."','".$_SERVER['HTTP_REFERER']."','".$_SERVER['REMOTE_ADDR']."',now(),'".$gubun."')";
+		$log_query	= "INSERT INTO tracking_info_9(tracking_media, tracking_refferer, tracking_ipaddr, tracking_date, tracking_gubun) values('".$_SESSION['ss_media']."','".$_SERVER['HTTP_REFERER']."','".$_SERVER['REMOTE_ADDR']."',now(),'".$gubun."')";
 		$q_result 	= mysqli_query($my_db, $log_query);
 
 		return $log_query;
