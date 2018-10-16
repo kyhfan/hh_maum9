caea6e4d71b47ada008bd0862124421849329e24
kyhfan <kyhfan@gmail.com>
2018-10-16 10:06:18 +0900

    4444

--- m/ajax_picture.php
+++ m/ajax_picture.php
@@ -25,18 +25,18 @@ foreach($verify_data as $key => $val)
 	if ($i == 3)
 		break;        
 
-	$htag_arr 	= explode(",",str_replace("#","",$val["verify_tag"]));
-//	if (!$htag_arr[0])
-		$htag_arr[0] = "마음봇건강키트";
+// 	$htag_arr 	= explode(",",str_replace("#","",$val["verify_tag"]));
+// //	if (!$htag_arr[0])
+// 		$htag_arr[0] = "마음봇건강키트";
 
-//	if (!$htag_arr[1])
-		$htag_arr[1] = "우리가족튼튼메신저";			
+// //	if (!$htag_arr[1])
+// 		$htag_arr[1] = "우리가족튼튼메신저";			
 
 	$val['verify_desc'] = str_replace('"','',$val['verify_desc']);
 	
 	$section1   .= "<div class='box' data-dynamic-flag='Y' data-popup='#popup-picture-detail' data-source-owner='".$val["verify_name"]."' data-source-url='../uploads/".$val["verify_directory"]."/".$val['verify_file_name']."' data-source-tag='".$htag_arr[0].",".$htag_arr[1]."' data-source-desc='".$val["verify_desc"]."'><div class='img'><img src='../uploads/".$val["verify_directory"]."/".$val["verify_file_name"]."'></div>";
 	
-	$section1   .= "<div class='hashtag'><span>#".$htag_arr[0]."</span><span>#".$htag_arr[1]."</span></div></div>";
+	// $section1   .= "<div class='hashtag'><span>#".$htag_arr[0]."</span><span>#".$htag_arr[1]."</span></div></div>";
 
 	$i++;
 }
@@ -54,7 +54,7 @@ foreach($insta_data as $key => $val) {
 	
 //	$section2   .= "<div class='box'><div class='img'><img src='".$val["standard_image"]."'></div>";
 	//	$section2   .= "<figure><div class='img'><a href='javascript:void(0)' onclick=open_detail('".$val["idx"]."');click_tracking('팝업-사진자세히보기')><img src='./uploads/".$val["verify_directory"]."/".$val["verify_file_name"]."'></div>";
-	$section2   .= "<div class='hashtag'><span>#".$htag_arr[0]."</span><span>#".$htag_arr[1]."</span></div></div>";
+	// $section2   .= "<div class='hashtag'><span>#".$htag_arr[0]."</span><span>#".$htag_arr[1]."</span></div></div>";
 }
 
 
