91af6c7a81e070a809a796c9a0ae1b302a82cd05
kyhfan <kyhfan@gmail.com>
2018-10-16 10:37:27 +0900

    11111

--- admin/head.php
+++ admin/head.php
@@ -101,10 +101,10 @@ if (isset($_SESSION['ss_mb_name']) == false)
                   <li>
                     <a href="daily_media_count.php">캠페인 참여자 수</a>
                   </li>
+                  <!-- <li>
+                    <a href="daily_media_count2.php">인증 이벤트 참여자 수</a>
+                  </li> -->
 <!--
-                  <li>
-                    <a href="daily_tracking_count2.php">2차 일자별 사이트 유입자 수</a>
-                  </li>
                   <li>
                     <a href="winner_count2.php">2차 경품별 당첨자 수</a>
                   </li>
